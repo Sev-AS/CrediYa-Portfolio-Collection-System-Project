d55bb7b235534316db0cc574843514275a07c7dd
Stiven <delleore1@gmail.com>
2025-12-10 12:53:44 -0500

    feat(UI) :sparkles: Actualizacion de Pagos Menu

--- src/main/java/com/crediya/pagos/PagosMenu.java
+++ src/main/java/com/crediya/pagos/PagosMenu.java
@@ -1,13 +1,16 @@
 package com.crediya.pagos;
 
+import java.util.Scanner;
+
 public class PagosMenu {
     private int opcion;
-
+    Scanner consola = new Scanner(System.in);
+    PagosGestor pg = new PagosGestor();
+    
+    @SuppressWarnings("OverridableMethodCallInConstructor")
     public PagosMenu(){
-        
-    }
+        pg.cargarPagos();
 
-    public void iniciarMenuClient(){
         do { 
             System.out.println(
                 """
@@ -29,7 +32,39 @@ public class PagosMenu {
 
         opcion = consola.nextInt();
 
+        switch (opcion){
+            case 1: MCAgregarPago(); pg.guardarPago();
+            break;
+            case 2: pg.listaDeTodosLosPagos();
+            break;
+        }
 
-        } while (!opcion == 4);
+        } while(opcion != 4);
+    }
+
+    public void MCAgregarPago(){
+
+        System.out.println("Dame el id");
+        int id = consola.nextInt();
+
+        System.out.println("Dame el id del prestamo");
+        int prestamo_id = consola.nextInt();
+
+        consola.nextLine();
+
+        System.out.println("Fecha de pago");
+        String fecha_pago = consola.nextLine();
+
+        System.out.println("Monto");
+        double monto = consola.nextDouble();
+
+        Pagos pago = new Pagos(id, prestamo_id, fecha_pago, monto);
+        pg.agregarPago(pago);
+    }
+
+    public static void main(String[] args) {
+        PagosMenu pm = new PagosMenu();
+
+        System.out.println(pm);
     }
 }
